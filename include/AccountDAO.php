ab37146ea12a1904d16f59d6f85e95ed92985adb
Brendon <brendonlim96@hotmail.com>
2020-01-18 16:35:31 +0800

    deleted validate.php file and moved it as a function in AccountDAO

--- include/AccountDAO.php
+++ include/AccountDAO.php
@@ -17,7 +17,6 @@ class AccountDAO {
         }
         return $result;
     }
-
     
     public function register($username,$password){
         $sql = 'INSERT INTO login_credentials(username,password), 
@@ -35,9 +34,26 @@ class AccountDAO {
         if ($stmt->execute()) {
             $isAddOK = True;    
         }
-        
         return $isAddOK;
     }
+
+    public function validateLogin($username, $password){
+        $sql = "SELECT username, password FROM login_credentials WHERE username='$username' AND password='$password'";
+        $connMgr = new ConnectionManager();      
+        $conn = $connMgr->getConnection();
+        $stmt = $conn->prepare($sql);
+        $stmt->execute();
+        $result = [];
+        while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+            $result[] = new Account($row['username'],$row['password']);
+        }
+        if (empty($result))
+            return False;
+        else  
+            return True;
+    }
+
+
 }
 
 
