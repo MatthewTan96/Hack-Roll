27f0fd1ddf61513b04620d51a19e283124208a19
MatthewTan96 <54927234+MatthewTan96@users.noreply.github.com>
2020-01-18 13:57:32 +0800

    Created Front page

---
+++ login.php
@@ -0,0 +1,50 @@
+Welcome to our sick ass game 
+
+<br>
+<br>
+<body>
+<h2> Login here: </h2>
+
+<form method='POST' action="validate.php">
+
+<!-- Username  -->
+    <label for="username">Username:</label>
+    <input type="username" placeholder='Enter Username' name="username" id="username">
+
+<!-- Password -->
+<br><br>     
+
+    <label for="password">Password:</label>
+    <input type="password" placeholder='Enter Password' class="password" name="password" id="password">
+
+<br><br>
+<button type="submit">Submit</button>
+
+
+<br><br>
+
+
+</form>
+
+<h2> New user? Register here: </h2>
+
+
+<form action="register.php" method="post">
+<label for="username"><b>Username:</b></label>
+<input type="text" placeholder="Enter Username" name="username" required>
+
+<br>
+<br>
+
+<label for="password"><b>Password:  </b></label>
+<input type="password" placeholder="Enter Password" name="password" required>
+
+<br>
+<button type="submit">Register</button>
+</form>
+
+
+
+<label>
+</form>
+</body>
