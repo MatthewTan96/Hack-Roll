59317c18b573e293f3397559c48aa0739c050587
MatthewTan96 <54927234+MatthewTan96@users.noreply.github.com>
2020-01-18 16:31:05 +0800

    Validation to home page

--- register.php
+++ register.php
@@ -19,14 +19,20 @@
     
     #Check if username is already on the database. Repeats not allowed 
 
-    foreach ($databaseUsernames as $usernames){
-        
+    foreach ($databaseUsernames as $databaseUsername){
+        if ($databaseUsername == $username){
+            $_SESSION["errors"] = "Username already taken!";
+            header('Location: login.php');
+        }
     }
     
+    #If username created, save username in a session and continue to the home page 
 
-    var_dump($databaseUsernames);
-
-    // $AccountDAO->register($)
+    $AccountDAO->register($username,$password);
     
+    $_SESSION('username') = $username;
+    $_SESSION('password') = $password;
+
+    header('Location: home.php');
 
 ?>
