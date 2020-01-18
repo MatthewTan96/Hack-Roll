80c3d903e8a6a612e809794a4c13eda3e6ea7002
MatthewTan96 <54927234+MatthewTan96@users.noreply.github.com>
2020-01-18 17:44:01 +0800

    whatever

--- login.php
+++ login.php
@@ -5,67 +5,126 @@
 
 ?>
 
-Welcome to NeuroParasite 
+<style> 
+body {
+  background-image: url("https://uqvk92z67p11sbpjb3nr4qo1-wpengine.netdna-ssl.com/wp-content/uploads/2019/06/apocalypse-1400x788.jpg");
+  background-size: cover;
+}
+
+.login {
+  color: white;
+  font-style: oblique;
+  font-size: 40px;
+  text-align: center;
+}
+
+.scroll {
+  text-align: center;
+}
+
+.credential{
+  text-align: center;
+  color: white;
+  font-size: 20px;
+}
+
+.center{
+  margin:auto;
+}
+
+.container{
+  background-color:black;
+  width: 40vw;
+  align-items: center;
+  opacity: .60;
+  
+
+}
+
+</style>
 
 
-<br>
-<br>
+<div class ="container">
+  <h1 class=login> Welcome to NeuroParasite </h1>
+
 <body>
-<h2> Login here: </h2>
+    
+    <h1 class=login> Login Access </h1>
 
-<form method='POST' action="validate.php">
+    <form method='POST' action="validate.php">
 
-<!-- Username  -->
-    <label for="username">Username:</label>
-    <input type="username" placeholder='Enter Username' name="username" id="username">
+    <!-- Username  -->
+    <div class=scroll>
+        <div class=credential> 
+            <label for="username">Username:</label>
+            <input type="username" placeholder='Enter Username' name="username" id="username">
 
-<!-- Password -->
-<br><br>     
-
-    <label for="password">Password:</label>
-    <input type="password" placeholder='Enter Password' class="password" name="password" id="password">
-
-<br><br>
-<button type="submit">Submit</button>
-
-
-<br><br>
-
-
-</form>
-
-<h2> New user? Register here: </h2>
-
-
-<form action="register.php" method="post">
-
-<!-- Username  -->
-<label for="username">Username:</label>
-<input type="username" placeholder='Enter Username' name="username" id="username">
-
-<!-- Password -->
-<br><br>     
-
-<label for="password">Password:</label>
-<input type="password" placeholder='Enter Password' class="password" name="password" id="password">
-
-<br><br>
-<button type="submit">Register</button>
-</form>
-
-<h2> Errors (if any): </h2>
-<?php
+        
+    <!-- Password -->
+    <br><br>     
+    <div style="text-align: center">
+        <label for="password">Password:</label>
+        <input type="password" placeholder='Enter Password' class="password" name="password" id="password">
+    
+    <div style="text-align: center">
+      <br><br>
+      <input type="submit" value="Login">
+    </div>
+  </div>
 
 
-if (isset($_SESSION["errors"])){
 
-    echo "<p> {$_SESSION['errors']} </p>";
+    <br><br>
 
-}
-else{
-    echo "No error!";
-}
-?>
+
+    </form>
+
+    <h1 class=login> New User? Register Here: </h1>
+
+
+    <div class=scroll>
+        <div class=credential> 
+            <form action="register.php" method="post">
+
+            <!-- Username  -->
+            <label for="username">Username:</label>
+            <input type="username" placeholder='Enter Username' name="username" id="username">
+
+            <!-- Password -->
+            <br><br>     
+
+            <label for="password">Password:</label>
+            <input type="password" placeholder='Enter Password' class="password" name="password" id="password">
+        </div>
+
+
+        <div style="text-align: center">
+      <br><br>
+      <input type="submit" value="Register">
+    </div>
+  </div>
+</div>
+    </form>
+
+    <!-- Display of errors -->
+<div class=scroll>
+    <div class=credential> 
+
+        <h2> Errors (if any): </h2>
+        <?php
+
+
+        if (isset($_SESSION["errors"])){
+
+            echo "<p> {$_SESSION['errors']} </p>";
+
+        }
+        else{
+            echo "No error!";
+        }
+        ?>
+    </div>
+</div>
 
 
 </body>
